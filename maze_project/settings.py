987548b1f03f2cb23004206d26d2489dd1b97099
fabimass <massotto.fabian@gmail.com>
2023-07-02 20:29:57 -0300

    removing ReactPy

--- maze_project/settings.py
+++ maze_project/settings.py
@@ -38,7 +38,6 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
-    "daphne",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- maze_project/urls.py
+++ maze_project/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("reactpy/", include("reactpy_django.http.urls")),
     path('', include("maze_app.urls"))
 ]
