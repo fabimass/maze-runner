099d430390f63b21b4284accb6f5a63c07743733
fabimass <massotto.fabian@gmail.com>
2023-07-02 17:20:06 -0300

    remove making migrations step

--- build.sh
+++ build.sh
@@ -13,10 +13,6 @@ python -m pip install --upgrade pip
 echo "Building the project..."
 python -m pip install -r requirements.txt
 
-# Make migrations
-echo "Making migrations..."
-python manage.py migrate --noinput
-
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
