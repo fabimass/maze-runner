19ac47f1e162b430e368aa8680b3f5821f06b56c
fabimass <massotto.fabian@gmail.com>
2023-07-02 20:04:24 -0300

    modify static files settings

--- maze_project/settings.py
+++ maze_project/settings.py
@@ -119,10 +119,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL="static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-# Configures the staticfiles directory to serve
-# static files from /static/ on our deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
