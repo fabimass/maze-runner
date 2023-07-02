a6122b1426d35c6d57eb396ca62a91a596445792
fabimass <massotto.fabian@gmail.com>
2023-07-02 16:22:07 -0300

    testing

--- maze_project/settings.py
+++ maze_project/settings.py
@@ -130,6 +130,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
