6f7db52a3f3add0bce78e641b549558f3d034e51
fabimass <massotto.fabian@gmail.com>
2023-07-02 15:08:38 -0300

    refactoring for vercel

--- maze_project/wsgi.py
+++ maze_project/wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for maze_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maze_project.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 application = get_wsgi_application()
+app = application
