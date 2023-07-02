f0a1aa16e20ae5259482f14bb57cdf8caa167e1b
fabimass <massotto.fabian@gmail.com>
2023-07-02 14:57:25 -0300

    basic textarea and button

--- maze_app/components.py
+++ maze_app/components.py
@@ -1,5 +1,14 @@
-from reactpy import component, html
+from reactpy import component, html, hooks
 
 @component
 def app():
-    return html.h1("Hello React Py!")
+
+    maze, set_maze = hooks.use_state("")
+
+    def handle_click(event):
+        print(event)
+
+    return html.div(
+        html.textarea({"onChange": lambda e: print(e['target']['value'])}),
+        html.button({"onClick": handle_click}, "Submit")
+    )
