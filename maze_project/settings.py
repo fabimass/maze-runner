0d164758491ffde7092e3803bdc14cc431ed9a26
fabimass <massotto.fabian@gmail.com>
2023-07-02 11:58:44 -0300

    completing initial setup

--- maze_project/settings.py
+++ maze_project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG")
+DEBUG = (os.getenv("DJANGO_DEBUG") == "True")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
