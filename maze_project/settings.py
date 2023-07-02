7b67e8f30bc5e62f22609c7157f8b8740199f6db
fabimass <massotto.fabian@gmail.com>
2023-07-02 15:37:51 -0300

    tweaking settings

--- maze_project/settings.py
+++ maze_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv("DJANGO_DEBUG") == "True")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'maze_project.wsgi.application'
+WSGI_APPLICATION = 'maze_project.wsgi.app'
 
 
 # Database
--- maze_project/wsgi.py
+++ maze_project/wsgi.py
@@ -1,5 +1,7 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maze_project.settings")
-application = get_wsgi_application()
-app = application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maze_project.settings')
+
+app = get_wsgi_application()
