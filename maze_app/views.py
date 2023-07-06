5f03862095589cc7cbaa6fe54bad5dfa3f035668
fabimass <massotto.fabian@gmail.com>
2023-07-06 17:48:22 -0300

    code for solving maze

--- maze_app/views.py
+++ maze_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def index(request):
     return render(request, "index.html")
+
+def run(request):
+    
+
+    return(JsonResponse({
+        "result": "..."
+        }, 
+        status=200))
