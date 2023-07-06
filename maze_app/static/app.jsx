df1ada1fffac7927ca539e99453d2e6a6bb7a5b3
fabimass <massotto.fabian@gmail.com>
2023-07-06 20:23:03 -0300

    echo between front and back

--- maze_app/static/app.jsx
+++ maze_app/static/app.jsx
@@ -1,7 +1,8 @@
 const App = () => {
 
+    const [mazeDesign, setMazeDesign] = React.useState();
     const [mazeSolution, setMazeSolution] = React.useState();
-
+    
     const runMaze = () => {
         
         fetch('/run', {
@@ -11,7 +12,7 @@ const App = () => {
                 'X-CSRFToken': CSRF_TOKEN
             },
             body: JSON.stringify({
-                maze: "..."
+                maze: mazeDesign
             }),
         })
         .then(response => {
@@ -36,7 +37,7 @@ const App = () => {
     }
 
     return <div>
-        <textarea></textarea>
+        <textarea onChange={(event) => setMazeDesign(event.target.value)}></textarea>
         <button onClick={runMaze}>Run Maze</button>
         <Blabla />
         <div>{mazeSolution}</div>
--- maze_app/views.py
+++ maze_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 def index(request):
     return render(request, "index.html")
 
 def run(request):
     
+    payload_data = json.loads(request.body)
+    print(payload_data)
     return(JsonResponse({
-        "result": "..."
+        "result": payload_data["maze"]
         }, 
         status=200))
