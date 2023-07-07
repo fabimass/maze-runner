f0875749140aa26c8ecdb6db33b6507d578da65d
fabimass <massotto.fabian@gmail.com>
2023-07-07 17:25:43 -0300

    adding colors to the maze

--- maze_app/static/mazePrint.jsx
+++ maze_app/static/mazePrint.jsx
@@ -2,9 +2,29 @@ const MazePrint = (props) => {
     
     return <div>
         { props.content ?
-            props.content.split('\n').map(row => <div className="flex">
-                {row.split("").map(cell => <div className="w-20 h-20 border-4"></div>)}
-            </div>)
+            props.content.split('\n').map(
+                row => <div className="flex">
+                    {row.split("").map(cell => {
+                        if (cell == '#'){
+                            return <div className="w-20 h-20 border-4 bg-slate-700"></div>
+                        }
+                        else if (cell == 'A'){
+                            return <div className="w-20 h-20 border-4 bg-teal-500"></div>
+                        }
+                        else if (cell == 'B'){
+                            return <div className="w-20 h-20 border-4 bg-green-500"></div>
+                        }
+                        else if (cell == '*'){
+                            return <div className="w-20 h-20 border-4 bg-violet-500"></div>
+                        }
+                        else if (cell == 'x'){
+                            return <div className="w-20 h-20 border-4 bg-red-500"></div>
+                        }
+                        else {
+                            return <div className="w-20 h-20 border-4"></div>
+                        }
+                    })}
+                </div>)
             : null }
     </div>
 }
